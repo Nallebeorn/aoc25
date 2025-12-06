5e1ecb7aac08b1fa8aa179d28fc87427f287879c
Benjamin <f.benjamin.larsson@gmail.com>
2025-12-06 16:28:27 +0100

    Day 06 part 2

--- day06/day06.py
+++ day06/day06.py
@@ -20,7 +20,37 @@ def part1(input: str):
     return result
 
 def part2(input: str):
-    ...
+    lines = [line + " " for line in input.splitlines()]
+    numbers = []
+    left_limit = 0
+    for i in range(len(lines[0])):
+        if all(line[i] == " " for line in lines):
+            nums = []
+            for j in range(left_limit, i):
+                n = ""
+                for row in range(len(lines) - 1):
+                    n += lines[row][j]
+                nums.append(int(n))
+            left_limit = i + 1
+            numbers.append(nums)
+
+    # print(numbers)
+        
+    operators = lines[-1].split()
+
+    result = 0
+    for i, op in enumerate(operators):
+        args = numbers[i]
+        match op:
+            case "+":
+                result += sum(args)
+            case "*":
+                result += prod(args)
+            case _:
+                raise
+        # print(i, op, args)
+    
+    return result
 
 
 if __name__ == "__main__":
@@ -33,8 +63,8 @@ if __name__ == "__main__":
     with open("input.txt", "r") as f:
         input = f.read()
         
-    print(part1(example))
-    print(part2(example))
+    # print(part1(example))
+    # print(part2(example))
 
     print(f"Part 1: {part1(input)}")
-    # print(f"Part 2: {part2(input)}")
+    print(f"Part 2: {part2(input)}")
