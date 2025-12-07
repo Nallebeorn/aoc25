26362f4e0382cebcec69f59a115e6ddb58ee6b97
Benjamin <f.benjamin.larsson@gmail.com>
2025-12-07 11:45:41 +0100

    Day 07 part 2

--- day07/day07.py
+++ day07/day07.py
@@ -27,7 +27,46 @@ def part1(input: str):
     return split_count
 
 def part2(input: str):
-    ...
+    lines = input.splitlines()
+    splitters = set[tuple[int, int]]()
+    startx = None
+    height = len(lines)
+
+    for y, line in enumerate(lines):
+        for x, c in enumerate(line):
+            match c:
+                 case "S":
+                    startx = x
+                 case "^":
+                    splitters.add((x, y))
+                 case ".":
+                    pass
+                 case _:
+                    raise
+
+    assert startx is not None
+
+    memo = {}
+    def explore_timeline(x: int, y: int, counter: int):
+        if y >= height:
+            return counter
+        
+        if (x, y) in memo:
+            return memo[(x, y)]
+        
+        if (x, y) in splitters:
+            counter = (
+                explore_timeline(x - 1, y + 1, counter)
+                +
+                explore_timeline(x + 1, y + 1, counter)
+            )
+        else:
+            counter = explore_timeline(x, y + 1, counter)
+
+        memo[(x, y)] = counter
+        return counter
+            
+    return explore_timeline(startx, 0, 1)
 
 
 if __name__ == "__main__":
@@ -52,7 +91,7 @@ if __name__ == "__main__":
     with open("input.txt", "r") as f:
         input = f.read()
         
-    print(part1(example))
+    # print(part1(example))
     # print(part2(example))
 
     print(f"Part 1: {part1(input)}")
