40d8718c47831fef756755ab3c5e1ed0a7747067
Benjamin <f.benjamin.larsson@gmail.com>
2025-12-10 07:15:52 +0100

    Part 2, WIP 2

--- day09/day09.py
+++ day09/day09.py
@@ -19,33 +19,30 @@ def part2(input: str):
     miny = min(y for x, y in red_tiles) - 1
     maxy = max(y for x, y in red_tiles) + 1
     
-    vertical_edges = set[tuple[int, int]]()
-    horizontal_edges = set[tuple[int, int]]()
-    for i, (x1, y1) in enumerate(red_tiles):
-        x2, y2 = red_tiles[(i+1) % len(red_tiles)]
-        if x1 == x2:
-            for y in range(min(y1, y2), max(y1, y2) + 1):
-                vertical_edges.add((x1, y))
-        elif y1 == y2:
-            for x in range(min(x1, x2), max(x1, x2) + 1):
-                horizontal_edges.add((x, y1))
+    edges = []
+    for i, a in enumerate(red_tiles):
+        b = red_tiles[(i+1) % len(red_tiles)]
+        edges.append((min(a, b), max(a, b)))
+
+    # print(edges)
 
     def is_point_in_green(px: int, py: int):
-        if (px, py) in vertical_edges or (px, py) in horizontal_edges:
-            return True
+        intersections = 0
+        intersections2 = 0
+        intersections3 = 0
+        intersections4 = 0
+        for (ax, ay), (bx, by) in edges:
+            if px > ax and px > bx and py >= ay and py < by:
+                intersections += 1
+            if px > ax and px > bx and py > ay and py <= by:
+                intersections2 += 1
+            if px >= ax and px >= bx and py >= ay and py < by:
+                intersections3 += 1
+            if px >= ax and px >= bx and py > ay and py <= by:
+                intersections4 += 1
+        
+        return intersections % 2 == 1 or intersections2 % 2 == 1 or intersections3 % 2 == 1 or intersections4 % 2 == 1
 
-        intersectionsx = 0
-        for x in range(px + 1, maxx):
-            if (x, py) in vertical_edges:
-                intersectionsx += 1
-
-        intersectionsy = 0
-        for y in range(py + 1, maxy):
-            if (px, y) in horizontal_edges:
-                intersectionsy += 1
-
-        return intersectionsx % 2 == 1 and intersectionsy % 2 == 1
-    
     def is_rectangle_all_green(x1, y1, x2, y2):
         for y in range(y1, y2):
             for x in range(x1, x2):
@@ -53,23 +50,23 @@ def part2(input: str):
                     return False
         return True
 
-    output = ""
-    for y in range(miny, maxy + 1):
-        for x in range(minx, maxx + 1):
-            # if (x, y) in vertical_edges or (x, y) in horizontal_edges:
-            #     if (x, y) in red_tiles:
-            #         output += "#"
-            #     else:
-            #         output += "X"
-            # elif is_point_in_green(x, y):
-            #     output += " "
-            if is_point_in_green(x, y):
-                output += "#"
-            else:
-                output += "."
-        output += "\n"
+    # output = ""
+    # for y in range(miny, maxy + 1):
+    #     for x in range(minx, maxx + 1):
+    #         # if (x, y) in vertical_edges or (x, y) in horizontal_edges:
+    #         #     if (x, y) in red_tiles:
+    #         #         output += "#"
+    #         #     else:
+    #         #         output += "X"
+    #         # elif is_point_in_green(x, y):
+    #         #     output += " "
+    #         if is_point_in_green(x, y):
+    #             output += "#"
+    #         else:
+    #             output += "."
+    #     output += "\n"
     
-    print(output)
+    # print(output)
 
     print("Finding largest valid rectangle...")
     largest_area = 0
@@ -79,7 +76,6 @@ def part2(input: str):
             y1, y2 = min(ay, by), max(ay, by) + 1
             area = (x2 - x1) * (y2 - y1)
             if area > largest_area and is_rectangle_all_green(x1, y1, x2, y2):
-                print(x1, y1, x2, y2)
                 largest_area = area
 
     return largest_area
@@ -103,4 +99,4 @@ if __name__ == "__main__":
     print(part2(example))
 
     print(f"Part 1: {part1(input)}")
-    # print(f"Part 2: {part2(input)}")
+    print(f"Part 2: {part2(input)}")
